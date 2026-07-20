f0b58964c8160c734259813f3d12065d89281e77
gnafisa513-oss <gnafisa513@gmail.com>
2026-07-20 10:29:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 API_TOKEN = "8621714095:AAGfkAvAnq0rCkUJs4AKo8o50js6q_Km7Ng"
-ADMIN_ID = 8676940332  # O'zingizning Telegram ID-ingizni yozing 
+ADMIN_ID = 519149626  # O'zingizning Telegram ID-ingizni yozing 
